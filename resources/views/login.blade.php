3be889c8d2779da75fb099ee84cba5c5b5955b27
FireNick44 | YannicA46 <yannic.studer44@gmail.com>
2023-03-09 18:15:40 +0100

    sync

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -11,50 +11,35 @@
 </head>
 <body>
     <section class="splitTwo">
-        div
-            logo
+        <div class="form">
+            <div class="splitTwoWrapper">
 
-            title = var xyz
+                <x-logo/>
+                <x-twoSplitTitle :title="$title = 'Login'" />
+                <x-formLogin/>
 
-            form
+                <section class="login">
 
+                        <form method="POST">
+                            <label for="email" style="margin-top: 1%">Email </label><br>
+                            <input type="email" id="email" name="email" class="inputBox"><br>
+                            <label for="password" style="margin-top: 1%">Password</label><br>
+                            <input type="password" id="password" name="password"><br>
 
+                            <div class="buttonSection">
+                                <input type="submit" value="log in">
+                                <a href="/register" class="registerButton">Register</a>
 
+                            </div>
+                        </form>
 
-        <section class="login">
-        
-            <x-logo/>
-            <x-twoSplitTitle :title="$title = 'Login'" />
-            
+                </section>
 
-            
-            <h1 class="loginTitel">Login</h1>
-        
-            <!--
-                Apple Login
-                Google Login
-            -->
-            
-                <form method="POST">
-                    <label for="email" style="margin-top: 1%"><strong>Email<strong> </label><br>
-                    <input type="email" id="email" name="email" class="inputBox"><br>
-                    <label for="password" style="margin-top: 1%">Password</label><br>
-                    <input type="password" id="password" name="password"><br>
-                
-                    <div class="buttonSection">
-                        <input type="submit" value="log in">
-                        <a href="/register" class="registerButton">Register</a>
+            </div>
+        </div>
+        <div class="picture">
 
-                    </div>
-                    
-        
-        
-                </form>
-            
-        </section>
-        <section class="picture">
-
-        </section>
+        </div>
     </section>
 </body>
 </html>
